02cc1691236378d864a9e91fa2d8bdd82f653e37
Jimena Sánchez <mjsp674@hotmail.com>
2025-01-13 15:26:03 -0500

    #00 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Jimena674.js
@@ -0,0 +1,27 @@
+//https://tc39.es/
+
+//Single line comments example
+
+/*
+Multi-line
+commets
+example
+*/
+
+var variable;
+const x = 1;
+let lesson;
+
+//Primitive values
+
+let nulo = nulo;
+let indefinido = undefined;
+let boolean = true;
+let numero = -1.56;
+let grandesEnteros = 9007199254740991n;
+let cadenasDeTexto = 'string';
+let simbolo = Symbol();
+
+//message to the console
+
+console.log('¡Hola, JavaScript!');
